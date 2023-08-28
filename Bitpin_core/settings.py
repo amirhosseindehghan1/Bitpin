70ac1a6bac7c876a26eb6fd3d98bb53183b1d2cb
amirhosseindehghan1 <dehghanamirhossein1@gmail.com>
2023-08-28 16:46:16 +0330

    Add Static

--- Bitpin_core/settings.py
+++ Bitpin_core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,30 +75,30 @@ WSGI_APPLICATION = 'Bitpin_core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bitpin',
-        'USER': 'root',
-        'PASSWORD': '123456789987654321',
-        'HOST': 'mariadb',
-        'PORT': '3306',
-        'OPTIONS' : {
-            'sql_mode' : 'STRICT_ALL_TABLES'
-        }
-    }
-}
-
-
-
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'bitpin',
+#         'USER': 'root',
+#         'PASSWORD': '123456789987654321',
+#         'HOST': 'mariadb',
+#         'PORT': '3306',
+#         'OPTIONS' : {
+#             'sql_mode' : 'STRICT_ALL_TABLES'
+#         }
 #     }
 # }
+
+
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
@@ -134,7 +134,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = 'static/'
 
 # Default primary key field type
